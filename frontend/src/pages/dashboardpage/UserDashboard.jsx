94c390543f8da5925a320f2c1449cb0b52e49d4f
Om Prakash Gehlot <opgehlot73@gamil.com>
2026-07-11 14:51:47 +0530

    commit

--- frontend/src/pages/dashboardpage/UserDashboard.jsx
+++ frontend/src/pages/dashboardpage/UserDashboard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import SearchBar from '../../components/SearchBar'
 import HeroCarousel from '../../components/HeroCarousel'
 import Navbar from '../../components/Navbar'
-import Profile from '../../components/profile'
+
 
 function UserDashboard() {
   return (
     <div>
    
          <Navbar/>
-         <Profile/>
+      
         
         
       
