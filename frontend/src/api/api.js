a7c47f576ed55f41c711f71e8efebaaa4cda3166
Om Prakash Gehlot <opgehlot73@gamil.com>
2026-09-17 11:16:08 +0530

    asfsdf

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // https://rodio-tradelink.onrender.com
-  baseURL: "https://rodio-tradelink.onrender.com/api",
+  baseURL: "/api",
 });
 
 API.interceptors.request.use((config) => {
